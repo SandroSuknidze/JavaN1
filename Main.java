30b788443578858899658b1999fb262bd0aa4a45
SandroSuknidze <84281442+SandroSuknidze@users.noreply.github.com>
2022-09-20 19:16:17 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input: ");
+        String output = scanner.nextLine();
+        System.out.println(output);
+    }
+}
